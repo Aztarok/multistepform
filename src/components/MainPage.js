11b1aa566dcc316f58937dd78dd80b1df7663d4e
Aztarok <aztarok2994@gmail.com>
2023-10-24 14:56:01 -0400

    Working on mobile view

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { ubuntu } from '@/app/fonts'
+import { ubuntu } from '@/app/fonts';
 import FirstStep from "./FirstStep";
 import SecondStep from "./SecondStep";
 import ThirdStep from "./ThirdStep";
@@ -26,12 +26,18 @@ const MainPage = () => {
     }
 
     return (
+
         <div className={`${ubuntu.className} flex items-center justify-center min-h-screen bg-gray-100`}>
-            <div className="flex gap-[10%] p-4 bg-white rounded-xl shadow-lg w-[70vw] h-[40vw]">
-                <div className="flex w-[30%] h-[100%] bg-red-50 rounded-md">
-                    <img src="./bg-sidebar-desktop.svg" alt="image for steps" className="object-cover w-full h-full rounded-lg" />
+            <div className="flex md:w-[30%] md:h-[100%] bg-red-50 md:rounded-xl overflow-hidden absolute top-0 left-0 rounded-none w-full sm:h-[40vw] bg-transparent">
+                    <img src="./bg-sidebar-desktop.svg" alt="image for steps" className="hidden object-cover rounded-lg" />
+                    <img src="./bg-sidebar-mobile.svg" alt="image for steps" className="relative object-cover w-full sm:block content-background" />
+                    
                 </div>
-                <div className={`${ubuntu.className} flex flex-col flex-1  max-w-[35vw] min-h-[35vw] self-center`}>
+            <div className="flex gap-[10%] p-4 bg-white rounded-xl shadow-lg w-[70vw] h-[40vw] content-area">
+                <div className="flex md:w-[30%] md:h-[100%] bg-red-50 md:rounded-xl overflow-hidden absolute top-0 left-0 rounded-none w-full bg-transparent">
+                    <img src="./bg-sidebar-desktop.svg" alt="image for steps" className="hidden object-cover rounded-lg" />
+                </div>
+                <div className={`${ubuntu.className} flex flex-col flex-1 max-w-[35vw] min-h-[35vw] self-center `}>
                     {change === 1 ? <FirstStep /> : null}
                     {change === 2 ? <SecondStep /> : null}
                     {change === 3 ? <ThirdStep /> : null}
